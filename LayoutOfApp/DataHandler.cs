05cf9003a67b949b290356aa9d0646fa1fcb2bf0
YuriUfimtsev <ufimtsev3007@gmail.com>
2022-12-09 18:48:16 +0300

    add untracked files

---
+++ LayoutOfApp/DataHandler.cs
@@ -0,0 +1,6 @@
+﻿namespace LayoutOfApp
+{
+    public class DataHandler
+    {
+    }
+}
---
+++ XLSXParser/EPPlusRealization.cs
@@ -0,0 +1,31 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XLSXParser;
+
+public class EPPlusRealization
+{
+    public void Process(Stream stream)
+    {
+        ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+        try
+        {
+            var package = new ExcelPackage(stream);
+            var sheet = package.Workbook.Worksheets[0];
+            for (var i = 1; i < 33; ++i)
+            {
+                var data = sheet.Cells[3, i].Value.ToString();
+                Console.WriteLine(data);
+            }
+        }
+
+        catch (Exception ex)
+        {
+            var c = ex;
+        }
+    }
+}
